875243842c3e1e64d2c8699696e99b758dcf1916
AyushiPatel <patelayushi1876@gmail.com>
2025-08-01 12:42:00 +0530

    done

--- js/Break_and_Continue.js
+++ js/Break_and_Continue.js
@@ -1,8 +1,18 @@
+//continue
 // Skipp the iterationnnn
+// for(let i=0; i<10; i++){
+//     if(i==5){
+//         continue;
+//     }
+//     console.log(i)
+// }
+
+//Break
+// Break the loop
 
 for(let i=0; i<10; i++){
-    if(i==5){
-        continue;
+    if(i>=5){
+        break;
     }
     console.log(i)
 }
