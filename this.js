eebdfbfb83b9086fd880a319a0206fe61e756f75
Ayushi patel <142826002+ayushi1807-tech@users.noreply.github.com>
2025-08-04 10:30:04 +0530

    Add files via upload

---
+++ this.js
@@ -0,0 +1,39 @@
+//this is not a variable. It is a keyword. You cannot change the value of this.
+//In JavaScript, the this keyword refers to an object.
+
+//this in a Method
+//When used in an object method, this refers to the object.
+const person = {
+    firstName: "John",
+    lastName: "Doe",    
+    id: 5566,
+    fullName : function(){
+        return this.firstName + " " + this.lastName; //this refers to the person object
+    }
+}
+console.log(person.fullName());
+
+
+// Using this in a Function
+// In a JavaScript function, the behavior of the this keyword varies depending on how the function is invoked.
+
+function greet() {
+    console.log('Hello, my name is ' + this.name);
+}
+const person1 = {
+    name: 'Amit',
+    sayHello: greet
+};
+const anotherPerson = {
+    name: 'Jatin'
+};
+person1.sayHello(); 
+
+// Default Binding
+// When this keyword is used in global scope this is set to window object.
+
+function verifyAge (){
+    const age = 22;
+    return age;
+}
+console.log(verifyAge());
